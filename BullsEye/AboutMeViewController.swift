e4db2ee9c466db64d020f60d84bdb834960d8ad9
Ervin Chow <ervinchow@gmail.com>
2019-07-13 17:49:29 -0700

    Add about author view

---
+++ BullsEye/AboutMeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutMeViewController.swift
+//  BullsEye
+//
+//  Created by Ervin Chow on 7/13/19.
+//  Copyright © 2019 Ervin Chow. All rights reserved.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeAboutMe(){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Ervin Chow on 7/13/19.
+//  Copyright © 2019 Ervin Chow. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
